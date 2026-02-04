34ddca367e297f42ee7ab1cb55fb6e586f46cda9
kato912 <89004444+kato912@users.noreply.github.com>
2026-02-05 01:21:44 +0700

    login system

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
+import { ExpressAuth } from '@auth/express';
 import express from 'express';
 import type { Express , Request, Response } from 'express';
+import { authConfig } from './config/auth';
 
 const app: Express = express();
 const port: number = 3000;
 
-app.get("/" , (req:Request , res:Response) => {
-    res.send('running');
+app.use("/api/auth",ExpressAuth(authConfig)); //signin / session
+
+app.get("/api" , (req:Request , res:Response) => {
+    res.send('api is running');
 });
 
 app.listen(port, () => {
--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -1,17 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import "dotenv/config";
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../../generated/prisma/client';
 
-const prismaClientSingleton = () => {
-    return new PrismaClient()
-}
+const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 5
+});
+const prisma = new PrismaClient({ adapter });
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClientSingleton | undefined
-}
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export { prisma }
