b0af810279853e6a8862cddd68c8231d5fb456f6
kato912 <89004444+kato912@users.noreply.github.com>
2026-02-05 13:48:15 +0700

    Middleware

---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response , NextFunction } from "express";
+import { getSession } from "@auth/express";
+import { authConfig } from "../config/auth";
+
+export const  requireAuth = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const session = await getSession(req,authConfig);
+        if (!session || !session.user) {
+            return res.status(401).json({ message: "Unauthorized Please Sign In" });
+            return;
+        }
+        res.locals.user = session.user;
+        next();
+    }catch(error){
+        console.error("Auth Middleware Error:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export async function requireAdmin(req: Request, res: Response, next: NextFunction) {
+    // เรียกใช้ requireAuth ก่อนเพื่อให้แน่ใจว่า Login แล้ว (หรือจะเรียกซ้อนกันใน Route ก็ได้)
+    const session = await getSession(req, authConfig);
+
+    if (!session || session.user?.role !== "ADMIN") {
+        res.status(403).json({ message: "Forbidden: Admins only" });
+        return;
+    }
+
+    next();
+}
