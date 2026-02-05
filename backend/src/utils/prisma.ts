4281be0c5dd045b1ab5ccb0a102b2679cda0d953
kato912 <89004444+kato912@users.noreply.github.com>
2026-02-05 12:58:19 +0700

    ...

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -7,7 +7,8 @@ const adapter = new PrismaMariaDb({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    connectionLimit: 5
+    connectionLimit: 5,
+    allowPublicKeyRetrieval: true
 });
 const prisma = new PrismaClient({ adapter });
 
