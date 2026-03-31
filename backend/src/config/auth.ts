6e7fe9928651d71ef77e23fe6563721238ebe496
kato912 <89004444+kato912@users.noreply.github.com>
2026-04-01 03:06:03 +0700

    N rob

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -6,8 +6,8 @@ export const authConfig = {
     adapter: PrismaAdapter(prisma),
     providers: [
         Google({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            clientId: process.env.AUTH_GOOGLE_ID!,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET!,
 
             profile(profile){
                 const isKKU = profile.email?.endsWith("@kkumail.com") || false;
