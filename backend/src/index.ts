f9f8b6118ded115809052067db10239eead94ca9
kato912 <89004444+kato912@users.noreply.github.com>
2026-04-01 03:39:46 +0700

    N rob

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,8 @@ import "dotenv/config";
 const app: Express = express();
 const port: number = 3000;
 
+app.set('trust proxy', 1);
+
 app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,              
